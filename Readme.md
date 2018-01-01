c234d141cb8879a98e63420e5a572c91fb6351fd
peterjewicz <peterjewicz@totalwebconnections.com>
2018-01-01 15:46:00 -0600

    comments an cleanup

--- Readme.md
+++ Readme.md
@@ -1,4 +1,37 @@
 *******STILL IN PROGRESS*********
 
 
-API examples for www.simpleleadtracker.com 
+API examples for www.simpleleadtracker.com
+
+
+There are two ways to make requests to the api
+
+
+1. Backend form process
+This allows you to create your own forms or other ways to process data before sending it to the Simple Lead Tracker Application
+
+2. Drop In form
+This requires no developer help and can be dropped into any web page.
+
+
+
+
+
+
+****BACKEND FORM PROCESS*****
+
+The backend form allows you to programatically send data to simple lead tracker. In order do so, you'll also need
+to send the secret key for the user along with the request to authenticate.
+
+Default field names are:
+contact
+company
+phone
+email
+category
+website
+
+
+Custom fields can also be send in the camel case
+
+For example, if a user has the field named "Customer Address" then sending the named parameter customerAddress will correctly save this value in the database
--- testPost.php
+++ testPost.php
@@ -1,6 +1,13 @@
 <?php
 
-$name = "Test Name";
+/**
+* Basic example of adding a lead programatically to the Simple Lead Tracker application
+* Secret key is required and is found on the settings page of the user
+*/
+
+
+//all values are optional
+$contact = "Test Name";
 $company = "Simple Lead Tracker";
 $phone = "555-555-5555";
 $email = "test@test.com";
@@ -9,28 +16,29 @@ $website = "www.simpleleadtracker.com";
 $secretKey = 'API Key Goes Here';
 
 
-$data = array('apiToken'=>$secretKey,
-              'name'=>$name,
+//create the data array 
+$data = array('apiToken'=>'keyGoesHere',
+              'contact'=>$contact,
               'company'=>$company,
               'phone'=>$phone,
               'email'=>$email,
               'category'=>$category,
               'website'=>$website
-          );
+ );
 
 
+//create a curl request to send the data to the sever
 $ch = curl_init();
-
 curl_setopt($ch, CURLOPT_URL,"https://www.simpleleadtracker.com/api/addLead");
 curl_setopt($ch, CURLOPT_POST, 1);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+curl_setopt($ch, CURLOPT_POSTFIELDS, $data);\
 
-$output= curl_exec ($ch);
+$output= curl_exec($ch);
+curl_close($ch);
 
-curl_close ($ch);
-
-// further processing ....
+//Output will contain array with index 0 value 200 on success and error code on failure
+//index 1 will contain more info
 var_dump($output);
 
 ?>
